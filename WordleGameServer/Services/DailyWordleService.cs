d6a2b6a0c51bb1ab31e02c21092b17915fc687db
SummerKearney314 <54952555+SummerKearney314@users.noreply.github.com>
2024-03-21 15:27:42 -0400

    created messages and rpc for the play and GetStats. Still needs to be implemented (look for the TODOs)

--- WordleGameServer/Services/DailyWordleService.cs
+++ WordleGameServer/Services/DailyWordleService.cs
@@ -1,8 +1,7 @@
 ﻿using Grpc.Core;
-using WordleGameServer.Protos;
 using Google.Protobuf.WellKnownTypes;
 using WordleGameServer.Clients;
-
+using WordleGameServer.Protos;
 
 namespace WordleGameServer.Services
 {
@@ -19,12 +18,25 @@ namespace WordleGameServer.Services
             try
             {
                 word = WordServiceClient.GetWord("Tests");
-            } catch (RpcException)
+            }
+            catch (RpcException)
             {
                 Console.WriteLine("Error The word server is currently unavaible");
             }
 
-            return Task.FromResult(new DailyWordleResponse { Word =  word});
+            return Task.FromResult(new DailyWordleResponse { Word = word });
+        }
+
+        public override async Task Play(IAsyncStreamReader<GuessRequest> requestStream, IServerStreamWriter<GuessResponse> responseStream, ServerCallContext context)
+        {
+            // TODO: bidirectional stream to run a complete game of Wordle
+
+        }
+
+        public override Task<WordStatsResponse> GetStats(WordStatsRequest request, ServerCallContext context)
+        {
+            // TODO: returns all the stored user-statistics for the current daily word 
+            return Task.FromResult(new WordStatsResponse { });
         }
     }
 }
